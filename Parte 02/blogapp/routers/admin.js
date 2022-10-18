0c3b0493244b1e43a2124d35dc01903e5539677f
Luiz Felipe Vieira <luizfelipevieira01@gmail.com>
2022-10-17 22:36:07 -0300

    Update files

--- Parte 02/blogapp/routers/admin.js
+++ Parte 02/blogapp/routers/admin.js
@@ -8,7 +8,10 @@ router.get('/posts', (req,res) => {
     res.send("Página de posts")
 })
 router.get('/categorias', (req,res) => {
-    res.send("Página de categorias")
+    res.render("admin/categorias")
 })
 
+router.get('/categorias/add', (req,res) => {
+    res.render("admin/addcategorias")
+})
 export default router; 
