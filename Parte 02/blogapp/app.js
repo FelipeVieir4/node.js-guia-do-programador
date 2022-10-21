ac50b578de2dc0322415decd1c1fbfbc298f6420
Luiz Felipe Vieira <luizfelipevieira01@gmail.com>
2022-10-20 22:11:39 -0300

    Update class 36

--- Parte 02/blogapp/app.js
+++ Parte 02/blogapp/app.js
@@ -1,7 +1,9 @@
 // Carregando módulos
     import express from 'express';
     import {engine} from 'express-handlebars';
-    import admin from './routers/admin.js'
+    import admin from './routers/admin.js';
+    import mongoose from 'mongoose'
+
     const app = express();
 
 //Configurações
@@ -20,7 +22,14 @@
     app.set('view engine', 'handlebars');
 
     //mongoose
-
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://localhost/blogapp", {
+        useNewUrlParser: true 
+    }).then(()=>{
+        console.log("MongoDB Conectado...")
+    }).catch((err)=>{
+        console.log("Houve um erro ao se conectar ao MongoDB: "+err)
+    });
     //Public -> para arquivos estáticos
     app.use(express.static("public"))
 
--- Parte 02/blogapp/routers/admin.js
+++ Parte 02/blogapp/routers/admin.js
@@ -1,6 +1,10 @@
 import express from 'express'
 const router = express.Router();
 
+import mongoose from 'mongoose'
+import '../models/Categoria.js'
+const Categoria = mongoose.model('categorias')
+
 router.get('/', (req,res) =>{
     res.render("admin/index")
 });
@@ -9,6 +13,20 @@ router.get('/posts', (req,res) => {
 })
 router.get('/categorias', (req,res) => {
     res.render("admin/categorias")
+})
+
+router.post('/categorias/nova', (req,res)=>{
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(()=>{
+        console.log("Categoria salva com sucesso!")
+    }).catch((err)=>{
+        console.log("Erro ao salvar categoria")
+    })
+
 })
 
 router.get('/categorias/add', (req,res) => {
