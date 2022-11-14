b076f4ef82bc6d2e05baf6e5b4f8de86295a98a5
Luiz Felipe Vieira <luizfelipevieira01@gmail.com>
2022-11-14 15:42:51 -0300

    update all file

--- Parte 02/blogapp/helpers/isAdmin.js
+++
@@ -1,7 +0,0 @@
-export function isAdmin(req, res, next) {
-        if(req.isAuthenticated() && req.user.isAdmin == 1){
-            return next();
-        }
-        req.flash("error_msg", "Você precisa ser um Admin")
-        res.redirect("/")
-    }
